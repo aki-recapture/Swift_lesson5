df4c1ba7069cd92d442665124fb64bf1915b9726
aki <64885025+aki-recapture@users.noreply.github.com>
2020-06-29 21:32:30 +0900

    Update StopWatchAppTests.swift

--- StopWatchApp/StopWatchAppTests/StopWatchAppTests.swift
+++ StopWatchApp/StopWatchAppTests/StopWatchAppTests.swift
@@ -2,8 +2,8 @@
 //  StopWatchAppTests.swift
 //  StopWatchAppTests
 //
-//  Created by 近藤明広 on 2020/06/10.
-//  Copyright © 2020 AkihiroKondo. All rights reserved.
+//  Created by xxxxx on 2020/06/10.
+//  Copyright © 2020 xxxxx. All rights reserved.
 //
 
 import XCTest
